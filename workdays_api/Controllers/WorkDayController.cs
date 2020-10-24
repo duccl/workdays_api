058d02a53d8a0a5fb80869446babdab0289a9ec1
Coltri <edr.ccol@gmail.com>
2020-10-24 12:50:00 -0300

    is_work_day check fixed || find workday after added

--- workdays_api/Controllers/WorkDayController.cs
+++ workdays_api/Controllers/WorkDayController.cs
@@ -21,29 +21,36 @@ namespace workdays_api.Controllers
 
         private bool Is_work_day(DateTime date)
         {
-            return DateSystem.IsPublicHoliday(date,CountryCode.BR) || date.DayOfWeek != DayOfWeek.Saturday || date.DayOfWeek != DayOfWeek.Sunday;        
+            return !DateSystem.IsPublicHoliday(date,CountryCode.BR) && 
+                   date.DayOfWeek != DayOfWeek.Saturday &&
+                   date.DayOfWeek != DayOfWeek.Sunday;        
         }
-        
-        private Models.WorkDay FindPastWorkDay(int past_days)
-        {
+
+        private Models.WorkDay FindWorkDay(int days){
             DateTime work_day = DateTime.Now;
-            int days_elapsed = 1;
-            while(days_elapsed != (past_days + 1))
-            {
-                if(Is_work_day(work_day)){
-                    work_day = work_day.AddDays(-days_elapsed);
-                    days_elapsed += 1;
+            int absolute_days_value = System.Math.Abs(days);
+            bool is_past = days < 0;
+            while(absolute_days_value != 0){
+                work_day = work_day.AddDays( is_past ? -1 : 1);
+                if( Is_work_day(work_day)){
+                    absolute_days_value -= 1;
                 }
             }
-            var work_day_class = new Models.WorkDay();
-            work_day_class.work_day = work_day;
-            return work_day_class;
+            var work_day_class_instance = new Models.WorkDay();
+            work_day_class_instance.work_day = work_day;
+            return work_day_class_instance;
         }
 
         [HttpGet("before/{past_days}")]
         public Models.WorkDay GetPastDays(int past_days)
         {
-            return FindPastWorkDay(past_days);
+            return FindWorkDay(-past_days);
+        }
+
+        [HttpGet("after/{next_days}")]
+        public Models.WorkDay GetNextDays(int next_days)
+        {
+            return FindWorkDay(next_days);
         }
     }
 }
